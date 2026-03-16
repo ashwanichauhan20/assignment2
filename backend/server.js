86a1f9a61f9dac3b9608b77df7119d6ca61a1759
Ashwani Chauhan <157831534+ashwanichauhan20@users.noreply.github.com>
2026-03-16 18:24:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,14 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "https://assignment2-frontend-0i3j.onrender.com",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}))
+
 app.use(express.json())
 
 connectDB()
