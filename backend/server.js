4fe080a6dcbe93d4516082702bb8b3bc2e5f2670
Ashwani Chauhan <157831534+ashwanichauhan20@users.noreply.github.com>
2026-03-19 16:56:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express();
+app.set("trust proxy", 1);
 
 // Database connection
 connectDB();
